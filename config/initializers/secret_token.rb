726da31906344b9d2828ff44b9b50b8e4360ff4a
Diego Achury <tiran_o@hotmail.com>
2013-04-15 15:57:45 -0500

    iniciando aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appinssol::Application.config.secret_token = 'c6378c39eb095e9c88ab72fb69be40e8bad400c4abc007874bab3a1b380ea66c05b61d8b6c29c1fd9b419f3d852130dbfa259cd863f472c6fb3f2de7b2dc2c25'
