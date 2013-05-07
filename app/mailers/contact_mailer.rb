0dcd3db452b52d6279930606975360bc5f959052
Diego Achury <tiran_o@hotmail.com>
2013-05-06 21:59:00 -0500

    creando y direccionandolos correos a comercial inssol y adicional mejorando el correo.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,13 @@
 class ContactMailer < ActionMailer::Base
-  default from: "dachury@lubu.com.co"
+  default from: "comercial@inssol.com.co"
 
  	 def contact_email(contact)
  	 	@contacto = contact
-  		mail(:to => contact.email, :subject => "peticion de usuario",  :bcc => ["tiran_o@hotmail.com", "Order Watcher <watcher@example.com>"])
+  		mail(:to => contact.email, :subject => "Solicitud de Contacto",  :bcc => ["comercial@inssol.com.co", "Solicitud de Contacto <comercial@inssol.com.co>"], :cc => "dachury@lubu.com.co")
   end
    def quote_email(quote)
  	 	@quote = quote
-  		mail(:to => quote.email, :subject => "peticion de usuario",  :bcc => ["tiran_o@hotmail.com", "Order Watcher <watcher@example.com>"])
+  		mail(:to => quote.email, :subject => "Solicitud de Cotizar",  :bcc => ["comercial@inssol.com.co", "Solicitud de Cotizar <comercial@inssol.com.co>"], :cc => "dachury@lubu.com.co")
   end
   
 end
