be63b5925224172739364dfed3288b0a58728f76
Diego Achury <tiran_o@hotmail.com>
2013-05-08 11:12:39 -0500

    inicializador de idiomas para eliminar error 500

--- config/application.rb
+++ config/application.rb
@@ -30,8 +30,8 @@ module Appinssol
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-     config.i18n.default_locale = :es
+     #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+     #config.i18n.default_locale = :es
 
      
 
