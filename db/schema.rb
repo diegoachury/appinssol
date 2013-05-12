bd89d165a9adf15008091996f9404e0446c839e0
Diego Achury <achurycompany@gmail.com>
2013-05-12 14:10:21 -0500

    adicionar a la tabla user registro

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130512154308) do
+ActiveRecord::Schema.define(:version => 20130512190147) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -126,6 +126,7 @@ ActiveRecord::Schema.define(:version => 20130512154308) do
     t.datetime "updated_at",                             :null => false
     t.string   "cargo"
     t.float    "cel"
+    t.float    "tel"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
