40d462a25cb5dbc7ad679b3a64d79638025e9522
diegoachury <tiran_o@hotmail.com>
2013-08-24 10:05:40 -0500

    creando catalogo de productos, mejorando vista de productos, catalago virtual

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ActiveRecord::Base
   belongs_to :category
-  attr_accessible :codigo_externo, :codigo_interno, :color, :descripcion, :ficha_produccion, :ficha_tecnica, :marca, :nombre, :referencia_marca, :talla
+  attr_accessible :codigo_externo, :imagen, :category_id, :codigo_interno, :color, :descripcion, :ficha_produccion, :ficha_tecnica, :marca, :nombre, :referencia_marca, :talla
+def self.search(search)
+	if search
+		find(:all, :conditions =>['nombre LIKE ? OR codigo_interno LIKE ? OR marca LIKE ? OR referencia_marca LIKE ?', "%#{search}%", "%#{search}%","%#{search}%", "%#{search}%"])
+		else
+			find(:all)
+	end	
+end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130823155525) do
+ActiveRecord::Schema.define(:version => 20130824113831) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(:version => 20130823155525) do
     t.integer  "category_id"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "imagen"
   end
 
   add_index "products", ["category_id"], :name => "index_products_on_category_id"
