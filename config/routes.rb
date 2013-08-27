a261e0f995d4e8c13aa6c933452ab08159b902d4
diegoachury <tiran_o@hotmail.com>
2013-08-27 15:39:28 -0500

    modificando ruta de error

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ match '/users/:id', :to => 'users#destroy', :as => :destroy_user, :via => :delet
   devise_for :users
 
   devise_scope :user do
-    match 'registrar', to: 'devise/registrations#new', as: :registrar
+    get 'registrar', to: 'devise/registrations#new', as: :registrar
     get 'login', to: 'devise/sessions#new', as: :login
     get 'logout', to: 'devise/sessions#destroy', as: :logout  
   end
