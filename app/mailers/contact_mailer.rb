4500f98e21961e4373e447854cddf966a2a4b4c0
diegoachury <tiran_o@hotmail.com>
2015-04-18 09:45:35 -0500

    ajustando error de envio de correo de contacto

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,11 +3,11 @@ class ContactMailer < ActionMailer::Base
 
  	 def contact_email(contact)
  	 	@contacto = contact
-  		mail(:to => contact.email, :subject => "Solicitud de Contacto",  :bcc => ["comercial@inssol.com.co", "Solicitud de Contacto <comercial@inssol.com.co>"], :cc => "dachury@lubu.com.co")
+  		mail(:to => contact.email, :subject => "Solicitud de Contacto",  :bcc => ["comercial@inssol.com.co", "Solicitud de Contacto <comercial@inssol.com.co>"], :cc => "achurycompany@gmail.com")
   end
    def quote_email(quote)
  	 	@quote = quote
-  		mail(:to => quote.email, :subject => "Solicitud de Cotizar",  :bcc => ["comercial@inssol.com.co", "Solicitud de Cotizar <comercial@inssol.com.co>"], :cc => "dachury@lubu.com.co")
+  		mail(:to => quote.email, :subject => "Solicitud de Cotizar",  :bcc => ["comercial@inssol.com.co", "Solicitud de Cotizar <comercial@inssol.com.co>"], :cc => "achurycompany@gmail.com")
   end
   
 end
