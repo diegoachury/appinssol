fe9c165583f60cc3891b193463d909603a970948
diegoachury <tiran_o@hotmail.com>
2015-04-18 10:30:34 -0500

    generando configuracion action mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "ventasinssol@gmail.com",
-  :password             => "chespirito12",
+  :password             => "qpugqlgntcskafym",
   :authentication       => "plain",
   :enable_starttls_auto => true
   }
