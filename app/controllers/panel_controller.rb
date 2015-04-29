0e3c8860a669b0c68333731775d51dd87bad4c44
diegoachury <tiran_o@hotmail.com>
2015-04-29 15:52:22 -0500

    error interno de consulta

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -2,7 +2,7 @@ class PanelController < ApplicationController
   def index
   	@users = User.limit(5)
   	@contacts = Contact.limit(10)
-  	@quotes = Quote.all
+  	@quotes = Quote.find(:all, :order => 'created_at DESC')
   end
   #eliminar usuarios
   def destroy
