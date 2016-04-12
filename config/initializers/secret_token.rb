d574b3a977623827607315694640696c539c3fda
diegoachury <tiran_o@hotmail.com>
2016-04-12 07:50:44 -0500

    ajuste secret token devise

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Appinssol::Application.config.secret_token = 'c6378c39eb095e9c88ab72fb69be40e8bad400c4abc007874bab3a1b380ea66c05b61d8b6c29c1fd9b419f3d852130dbfa259cd863f472c6fb3f2de7b2dc2c25'
+Appinssol::Application.config.secret_token = 'c6378c39eb095e9c88ab72fb6'
