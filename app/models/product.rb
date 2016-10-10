d2bbf368dbeb9cb0b45cae464a715e359079ae48
diegoachury <tiran_o@hotmail.com>
2016-10-10 10:50:55 -0500

    ajuste de busqueda

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,17 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   attr_accessible :codigo_externo, :imagen, :precio, :category_id, :codigo_interno, :color, :descripcion, :ficha_produccion, :ficha_tecnica, :marca, :nombre, :referencia_marca, :talla
-def self.search(search, page)
-	paginate :per_page => 8, :page => page,
-	:conditions =>['nombre LIKE ?', "%#{search}%"],
-	:order => 'nombre'
-end
+# def self.search(search, page)
+# 	paginate :per_page => 8, :page => page,
+# 	:conditions =>['nombre LIKE ? OR codigo_interno LIKE ? OR marca LIKE ? OR referencia_marca LIKE ?', "%#{search}%", "%#{search}%","%#{search}%", "%#{search}%"],
+# 	:order => 'nombre'
+# end
+def self.search(search, page = 1 )
+   wildcard_search = "%#{search}%"
+
+       where("nombre LIKE :search", search: wildcard_search)
+         .page(page)
+         .per_page(5)
+ end
 
 end
