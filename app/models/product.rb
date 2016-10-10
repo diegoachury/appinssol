eecc0788428419a27b48407fecb0f0284ddf3d37
diegoachury <tiran_o@hotmail.com>
2016-10-10 10:28:14 -0500

    error de la busqueda

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   attr_accessible :codigo_externo, :imagen, :precio, :category_id, :codigo_interno, :color, :descripcion, :ficha_produccion, :ficha_tecnica, :marca, :nombre, :referencia_marca, :talla
 def self.search(search, page)
 	paginate :per_page => 8, :page => page,
-	:conditions =>['nombre LIKE ? OR codigo_interno LIKE ? OR marca LIKE ? OR referencia_marca LIKE ?', "%#{search}%", "%#{search}%","%#{search}%", "%#{search}%"],
+	:conditions =>['nombre LIKE ?', "%#{search}%"],
 	:order => 'nombre'
 end
 
